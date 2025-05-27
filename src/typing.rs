44fa5630baddafa0b8a158f67381646c02df155a
binary-song <yeh189@126.com>
2025-05-27 22:04:03 +0800

    Fix the long standing `unfiy` bug

--- src/typing.rs
+++ src/typing.rs
@@ -1,4 +1,5 @@
 use crate::ast::*;
+use crate::compat_serialize::{Span, TreeContext};
 use crate::{
     ast::{Lit, Term, Ty},
     compat_serialize::IntoTreeWithContext,
@@ -9,7 +10,6 @@ use std::{
     collections::{HashMap, HashSet, VecDeque},
     fmt::{format, Debug},
 };
-const log_enabled: bool = false;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum PrimitiveType {
@@ -26,18 +26,31 @@ pub enum Type {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Context {
+struct Context<'source> {
     var_types: VecDeque<(String, Type)>,
     last_var_name: i32,
     constraints: Vec<(Type, Type)>,
+    recursion: i32,
+    source: Option<&'source str>,
 }
 
-impl Context {
-    fn empty() -> Self {
+impl<'source> Context<'source> {
+    fn new_with_source(source: &'source str) -> Self {
         Context {
             var_types: VecDeque::new(),
             last_var_name: 0,
             constraints: vec![],
+            recursion: 0,
+            source: Some(source),
+        }
+    }
+    fn new() -> Self {
+        Context {
+            var_types: VecDeque::new(),
+            last_var_name: 0,
+            constraints: vec![],
+            recursion: 0,
+            source: None,
         }
     }
 }
@@ -65,7 +78,16 @@ fn syntax_type_to_real_type(ty: Ty) -> Type {
 }
 
 fn type_of(term: Term, context: &mut Context) -> Type {
-    match term {
+    if let Some(src) = context.source {
+        debug!(
+            "[{recur}]{spaces}typing: {term}",
+            recur = context.recursion,
+            spaces = " ".repeat(context.recursion as usize),
+            term = &src[term.span().0..term.span().1]
+        );
+    }
+    context.recursion += 1;
+    let ty = match term {
         Term::Lit { value, span } => match value {
             Lit::Unit => Type::Primitive(PrimitiveType::Unit),
             Lit::Int(_) => Type::Primitive(PrimitiveType::Int),
@@ -79,18 +101,20 @@ fn type_of(term: Term, context: &mut Context) -> Type {
             .1
             .clone(),
         Term::App { callee, args, span } => {
-            let result_rhs_type = fresh_var(context);
             let callee_type = type_of(*callee, context);
             let arg_types = args
                 .iter()
                 .map(|arg| type_of(arg.clone(), context))
                 .collect::<Vec<_>>();
-            let res_type = Type::Func {
+            let rhs_type = fresh_var(context);
+            let func_type = Type::Func {
                 lhs: arg_types,
-                rhs: Box::new(result_rhs_type),
+                rhs: Box::new(rhs_type.clone()),
             };
-            constrain(callee_type, res_type.clone(), context);
-            res_type
+            // constraint: callee should be a function that
+            // happen to accept the exact arg types.
+            constrain(callee_type, func_type.clone(), context);
+            rhs_type
         }
         Term::Func {
             params,
@@ -135,15 +159,17 @@ fn type_of(term: Term, context: &mut Context) -> Type {
         } => {
             // type the rhs
             let rhs_ty = type_of(*rhs, context);
-            // if annotated, use the type
-            let ty = match ty {
-                Some(ty) => syntax_type_to_real_type(ty),
-                None => fresh_var(context),
+            // if annotated, constrain the rhs to the annotated type
+            let rhs_ty = match ty {
+                Some(anno_ty) => {
+                    let anno_ty = syntax_type_to_real_type(anno_ty);
+                    constrain(rhs_ty.clone(), anno_ty, context);
+                    rhs_ty
+                }
+                None => rhs_ty,
             };
-            // constrain the rhs to the type
-            constrain(rhs_ty, ty.clone(), context);
             // add the var to the context
-            context.var_types.push_front((name.clone(), ty));
+            context.var_types.push_front((name.clone(), rhs_ty));
             // type the body
             let body_ty = type_of(*body, context);
             // pop the context
@@ -159,38 +185,33 @@ fn type_of(term: Term, context: &mut Context) -> Type {
             lhs: vec![Type::Primitive(PrimitiveType::Int)],
             rhs: Box::new(Type::Primitive(PrimitiveType::Int)),
         },
-    }
+    };
+    context.recursion -= 1;
+    debug!(
+        "[{recur}]{spaces}result: {ty:?}",
+        recur = context.recursion,
+        spaces = " ".repeat(context.recursion as usize),
+    );
+    ty
 }
 
-fn apply_solution(ty: Type, sln: &HashSet<(Type, Type)>) -> Type {
-    match ty {
-        Type::Primitive(p) => Type::Primitive(p),
-        Type::Func { lhs, rhs } => {
-            let new_lhs = lhs.into_iter().map(|t| apply_solution(t, sln)).collect();
-            let new_rhs = apply_solution(*rhs, sln);
-            Type::Func {
-                lhs: new_lhs,
-                rhs: Box::new(new_rhs),
-            }
-        }
-        var @ Type::Var { name } => {
-            if let Some((_, sln_dst)) = sln.iter().find(|(sln_src, _)| sln_src == &var) {
-                sln_dst.clone()
-            } else {
-                var
-            }
-        }
-    }
-}
-
-fn type_term(term: Term) -> Option<Type> {
-    let mut context = Context::empty();
+fn type_term_with_context(term: Term, mut context: Context) -> Option<Type> {
     let ty = type_of(term, &mut context);
-    let constraints = unify(context.constraints.into_iter().collect())?;
+    let subst = unify(context.constraints.into_iter().collect())?;
     debug!("raw type = {ty}", ty = ty.dbg_print_root());
-    let ty = apply_solution(ty, &constraints);
+    let ty = subst.apply(ty);
     debug!("final type: {ty}", ty = ty.dbg_print_root());
     return Some(ty);
+}
+
+pub fn type_term(term: Term) -> Option<Type> {
+    let context = Context::new();
+    type_term_with_context(term, context)
+}
+
+pub fn type_term_with_source<'source>(term: Term, source: &'source str) -> Option<Type> {
+    let context = Context::new_with_source(source);
+    type_term_with_context(term, context)
 }
 
 fn fv(ty: &Type) -> Vec<Type> {
@@ -208,54 +229,84 @@ fn fv(ty: &Type) -> Vec<Type> {
     }
 }
 
-fn replace_constraints(
-    constraints: HashSet<(Type, Type)>,
-    from: &Type,
-    to: &Type,
-) -> HashSet<(Type, Type)> {
-    let mut result: HashSet<(Type, Type)> = HashSet::new();
-    for (lhs, rhs) in constraints {
-        let new_lhs = if lhs == *from { to.clone() } else { lhs };
-        let new_rhs = if rhs == *from { to.clone() } else { rhs };
-        result.insert((new_lhs, new_rhs));
-    }
-    result
+struct Substitution {
+    replacements: Vec<(i32, Type)>,
 }
 
-/// Given substitutions σ and γ, calculate the composite substitution σ ∘ γ
-/// where σ is given by `sigma`, and γ is {`gamma_src` -> `gamma_dst`}
-fn compose_substitutions(
-    sigma: HashSet<(Type, Type)>,
-    gamma_src: Type,
-    gamma_dst: Type,
-) -> HashSet<(Type, Type)> {
-    let mut res: HashSet<(Type, Type)> = sigma
-        .iter()
-        .map(|(src, dst)| {
-            if src != &gamma_src {
-                (src.clone(), dst.clone())
-            } else {
-                match sigma.iter().find(|(a, _)| a == &gamma_dst) {
-                    Some((_, b)) => (src.clone(), b.clone()),
-                    None => (src.clone(), gamma_dst.clone()),
+impl DebugPrint for Substitution {
+    fn dbg_print_raw(&self, ctx: &DebugPrintContext) -> String {
+        if self.replacements.is_empty() {
+            return "∅".to_string();
+        }
+        self.replacements
+            .iter()
+            .rev()
+            .map(|(from, to)| format!("T{from} => {to}", from = from, to = to.dbg_print(ctx)))
+            .collect::<Vec<_>>()
+            .join("; ")
+    }
+
+    fn precedence(&self) -> i32 {
+        -1
+    }
+}
+
+impl Substitution {
+    fn new() -> Self {
+        Substitution {
+            replacements: vec![],
+        }
+    }
+
+    /// Push a new substitution rule `from` -> `to` into the stack.
+    fn push(&mut self, from: i32, to: Type) {
+        self.replacements.push((from, to));
+    }
+
+    /// Apply the substitution stack from top to bottom.
+    fn apply(&self, ty: Type) -> Type {
+        fn apply_one(ty: Type, from: i32, to: &Type) -> Type {
+            match ty {
+                Type::Primitive(_) => ty,
+                Type::Func { lhs, rhs } => {
+                    let new_lhs = lhs
+                        .into_iter()
+                        .map(|t| apply_one(t, from, to))
+                        .collect::<Vec<_>>();
+                    let new_rhs = Box::new(apply_one(*rhs, from, to));
+                    Type::Func {
+                        lhs: new_lhs,
+                        rhs: new_rhs,
+                    }
+                }
+                ty @ Type::Var { name } => {
+                    if from == name {
+                        to.clone()
+                    } else {
+                        ty
+                    }
                 }
             }
-        })
-        .collect();
-    if res.iter().find(|(a, b)| a == &gamma_src).is_none() {
-        res.insert((gamma_src.clone(), gamma_dst.clone()));
+        }
+        let mut ty = ty;
+        for (from, to) in self.replacements.iter().rev() {
+            ty = apply_one(ty, *from, to);
+        }
+        ty
     }
-    res
 }
 
-fn unify_with_rec(c: HashSet<(Type, Type)>, recursion: i32) -> Option<HashSet<(Type, Type)>> {
-    fn unify_with_rec_impl(
-        mut c: HashSet<(Type, Type)>,
-        recursion: i32,
-    ) -> Option<HashSet<(Type, Type)>> {
+fn unify_with_rec(c: HashSet<(Type, Type)>, recursion: i32) -> Option<Substitution> {
+    let mut c = c;
+    log::debug!(
+        "[{recursion}]{spaces} unifying {tree}",
+        tree = c.dbg_print_root(),
+        spaces = " ".repeat(recursion as usize)
+    );
+    let result = {
         let first = c.iter().next().cloned();
         if first.is_none() {
-            return Some(HashSet::new());
+            return Some(Substitution::new());
         }
         let first = first.unwrap();
         c.remove(&first);
@@ -264,12 +315,22 @@ fn unify_with_rec(c: HashSet<(Type, Type)>, recursion: i32) -> Option<HashSet<(T
             return unify_with_rec(c, recursion + 1);
         }
         match (lhs, rhs) {
-            (x @ Type::Var { .. }, t) | (t, x @ Type::Var { .. }) if !fv(&t).contains(&x) => {
-                let c = replace_constraints(c, &x, &t);
-                let c = unify_with_rec(c, recursion + 1)?;
-                // ?? Subst != Constr !!!
-                let c = compose_substitutions(c, x, t);
-                Some(c)
+            (x @ Type::Var { name }, t) | (t, x @ Type::Var { name }) if !fv(&t).contains(&x) => {
+                debug!(
+                    "[{recursion}]{spaces} replacing {x} with {t}",
+                    x = x.dbg_print_root(),
+                    t = t.dbg_print_root(),
+                    spaces = " ".repeat(recursion as usize)
+                );
+                let mut x_to_t = Substitution::new();
+                x_to_t.push(name, t.clone());
+                let c = c
+                    .iter()
+                    .map(|(s, t)| (x_to_t.apply(s.clone()), x_to_t.apply(t.clone())))
+                    .collect::<HashSet<_>>();
+                let mut subs = unify_with_rec(c, recursion + 1)?;
+                subs.push(name, t);
+                Some(subs)
             }
             (
                 Type::Func {
@@ -289,22 +350,16 @@ fn unify_with_rec(c: HashSet<(Type, Type)>, recursion: i32) -> Option<HashSet<(T
             }
             _ => None,
         }
-    }
+    };
     log::debug!(
-        "[{recursion}]{spaces} unifying {tree}",
-        tree = c.dbg_print_root(),
-        spaces = " ".repeat(recursion as usize)
-    );
-    let result = unify_with_rec_impl(c, recursion);
-    log::debug!(
-        "[{recursion}]{spaces} unify result {result}",
+        "[{recursion}]{spaces} unify result: {result}",
         spaces = " ".repeat(recursion as usize),
         result = result.dbg_print_root()
     );
     result
 }
 
-fn unify(c: HashSet<(Type, Type)>) -> Option<HashSet<(Type, Type)>> {
+fn unify(c: HashSet<(Type, Type)>) -> Option<Substitution> {
     log::debug!("unify starts");
     let res = unify_with_rec(c, 1);
     log::debug!("unify final result: {res}", res = res.dbg_print_root());
@@ -366,7 +421,7 @@ impl DebugPrint for Type {
                     format!("({lhs_str}) -> {}", rhs.dbg_print(ctx))
                 }
             }
-            Type::Var { name } => format!("?{name}"),
+            Type::Var { name } => format!("T{name}"),
         }
     }
 }
@@ -385,7 +440,7 @@ impl DebugPrint for HashSet<(Type, Type)> {
                 )
             })
             .collect::<Vec<_>>()
-            .join(", ")
+            .join("; ")
     }
     fn precedence(&self) -> i32 {
         -1
@@ -411,7 +466,7 @@ where
 }
 
 #[cfg(test)]
-fn test_unify(source: &str) {
+fn test_unify(source: &str, expected: Option<Type>) {
     use crate::ast::Term;
     use crate::compat_serialize::TreeContext;
     use crate::parser::parse;
@@ -432,7 +487,8 @@ fn test_unify(source: &str) {
                 version: 1
             })
     );
-    let ty: Type = type_term(term).unwrap();
+    let ty = type_term_with_source(term, source);
+    assert_eq!(ty, expected,);
 }
 
 #[test]
@@ -446,5 +502,35 @@ fn unify_test1() {
     b
 }
     ",
+        Some(Type::Primitive(PrimitiveType::Int)),
     )
 }
+
+#[test]
+fn unify_test2() {
+    test_unify(
+        "
+{
+    let x = 1;
+    let f = fn (x: bool) { x };
+    f(x)
+}
+    ",
+        None,
+    )
+}
+
+#[test]
+fn unify_test3() {
+    test_unify(
+        "
+{
+    let x = 1;
+    let f = fn (x) -> bool { x };
+    f(x)
+}
+    ",
+        None,
+    )
+}
+
