b52630c32bde8bc274ff451066f6e3a4c973d404
binary-song <yeh189@126.com>
2025-06-09 20:55:42 +0800

    Syntax file is now generated

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
+use dough_utils::syntax::syntax_to_file;
+use std::path::Path;
 fn main() {
-    lalrpop::process_root().unwrap();
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR not set");
+    let rule_path = format!("{}/syntax.lalrpop", out_dir);
+    syntax_to_file(&rule_path);
+    lalrpop::Configuration::new()
+        .process_file(rule_path)
+        .unwrap();
 }
